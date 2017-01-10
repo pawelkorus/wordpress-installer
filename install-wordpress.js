00f6f1dc57445cb70088098ccc0b2fa615ccbf53
Pawel Korus <pawelkorus@ymail.com>
2017-01-10 14:13:49 +0100

    creating wp-config

--- install-wordpress.js
+++ install-wordpress.js
@@ -1,5 +1,10 @@
 var installer = require('./lib/installer.js');
 installer({
 	root: 'wordpress-latest',
-	version: 'latest'
-}).ensure();
+	version: 'latest',
+	dbName: 'dbDupa',
+	dbUser: 'dbUser',
+	dbPassword: 'dbPassword',
+	dbHost: 'localhost',
+	tablePrefix: 'tablePrefix'
+}).createWpConfig();
